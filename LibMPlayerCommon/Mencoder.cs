cda5c1c7cf34e1ab9a9ecc2088f1e4704ab91713
Peter Gill <peter@majorsilence.com>
2012-02-24 21:52:28 -0330

    Change license to GPL2 and LGPL2 to match license version mplayer is using.
    Was 3 before the change.

--- LibMPlayerCommon/Mencoder.cs
+++ LibMPlayerCommon/Mencoder.cs
@@ -1,9 +1,30 @@
-﻿using System;
+﻿/*
+
+Copyright 2012 (C) Peter Gill <peter@majorsilence.com>
+
+This file is part of LibMPlayerCommon.
+
+LibMPlayerCommon is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+LibMPlayerCommon is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using System.Text;
+
 
 namespace LibMPlayerCommon
 {
