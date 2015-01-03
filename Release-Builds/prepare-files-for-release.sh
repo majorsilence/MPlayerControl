28c207e447fedd83a37de91e75c6b69a3558a201
Peter Gill <peter@majorsilence.com>
2015-01-03 15:16:35 -0330

    script to increase version numbers for new releases

---
+++ Release-Builds/prepare-files-for-release.sh
@@ -0,0 +1,28 @@
+
+CURRENTPATH=`pwd`
+
+# Increment minor number by 1
+# See http://www.codeproject.com/Articles/31236/How-To-Update-Assembly-Version-Number-Automaticall
+mono ./tools/AssemblyInfoUtil.exe -inc:2 "$CURRENTPATH/../LibImages/Properties/AssemblyInfo.cs"
+mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../LibMPlayerCommon/Properties/AssemblyInfo.cs"
+mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../MediaPlayer/Properties/AssemblyInfo.cs"
+mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../LibMPlayerWinform/Properties/AssemblyInfo.cs"
+mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../MPlayerGtkWidget/AssemblyInfo.cs"
+mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../SlideShow/Properties/AssemblyInfo.cs"
+mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../WpfMPlayer/Properties/AssemblyInfo.cs"
+
+
+VERSION_FULL_LINE=$(grep -F "AssemblyVersion" "$CURRENTPATH/../LibMPlayerCommon/Properties/AssemblyInfo.cs")
+VERSION_WITH_ASTERIK=$(echo $VERSION_FULL_LINE | awk -F\" '{print $(NF-1)}')
+VERSION="${VERSION_WITH_ASTERIK%??}"
+
+
+cd ..
+cd Release-Builds
+
+REM Update NUGET spec files
+sed -i "5s/.*/ <version>$VERSION<\/version>/" "$CURRENTPATH\/nuget\/MPlayerControl\/MPlayerControl.nuspec"
+sed -i "5s/.*/ <version>$VERSION<\/version>/" "$CURRENTPATH\/nuget\/MPlayerControl-Winform\/MPlayerControl-Winform.nuspec"
+sed -i "5s/.*/ <version>$VERSION<\/version>/" "$CURRENTPATH\/nuget\/MPlayerControl-Gtk\/MPlayerControl-Gtk.nuspec"
+
+del "sed*"
