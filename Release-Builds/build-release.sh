875fe7bf3304b914624e3c00e72b93a6b5681c6d
Peter Gill <peter@majorsilence.com>
2015-01-04 16:35:48 -0330

    Include xml doc in nuget packages.

--- Release-Builds/build-release.sh
+++ Release-Builds/build-release.sh
@@ -14,8 +14,10 @@ mkdir -p ./build-output/MPlayerControl-dot-net-4.5-AnyCPU
 
 cp ../MediaPlayer/bin/Release/LibImages.dll "./build-output/$PACKAGEDIR/LibImages.dll"
 cp ../MediaPlayer/bin/Release/LibMplayerCommon.dll "./build-output/$PACKAGEDIR/LibMplayerCommon.dll"
+cp ../MediaPlayer/bin/Release/LibMplayerCommon.xml "./build-output/$PACKAGEDIR/LibMplayerCommon.xml"
 cp ../MediaPlayer/bin/Release/MediaPlayer.exe "./build-output/$PACKAGEDIR/MediaPlayer.exe"
 cp ../LibMPlayerWinform/bin/Release/LibMPlayerWinform.dll "./build-output/$PACKAGEDIR/LibMPlayerWinform.dll"
+cp ../LibMPlayerWinform/bin/Release/LibMPlayerWinform.xml "./build-output/$PACKAGEDIR/LibMPlayerWinform.xml"
 cp ../MPlayerGtkWidget/bin/Release/MPlayerGtkWidget.dll "./build-output/$PACKAGEDIR/MPlayerGtkWidget.dll"
 cp ../SlideShow/bin/Release/SlideShow.exe "./build-output/$PACKAGEDIR/SlideShow.exe"
 
@@ -37,6 +39,7 @@ mkdir content
 
 cp "$CURRENTPATH/build-output/$PACKAGEDIR/LibImages.dll" lib/net45/LibImages.dll
 cp "$CURRENTPATH/build-output/$PACKAGEDIR/LibMplayerCommon.dll" lib/net45/LibMplayerCommon.dll
+cp "$CURRENTPATH/build-output/$PACKAGEDIR/LibMplayerCommon.xml" lib/net45/LibMplayerCommon.xml
 cp "$CURRENTPATH/build-output/$PACKAGEDIR/MediaPlayer.exe" content/MediaPlayer.exe
 
 nuget pack "$CURRENTPATH/nuget/MPlayerControl/MPlayerControl.nuspec" -OutputDirectory "$CURRENTPATH/build-output"
@@ -52,6 +55,7 @@ rm -rf lib/net45
 mkdir -p lib/net45
 
 cp "$CURRENTPATH/build-output/$PACKAGEDIR/LibMPlayerWinform.dll" lib/net45/LibMPlayerWinform.dll
+cp "$CURRENTPATH/build-output/$PACKAGEDIR/LibMPlayerWinform.xml" lib/net45/LibMPlayerWinform.xml
 
 nuget pack "$CURRENTPATH/nuget/MPlayerControl-Winform/MPlayerControl-Winform.nuspec" -OutputDirectory "$CURRENTPATH/build-output"
 cd "$CURRENTPATH"
