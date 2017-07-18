ed8ce472ea6cf95d5b0fc0fda895de253cfb8934
Peter Gill <peter.gill@townsuite.com>
2017-07-18 08:53:34 -0230

    bump version to 1.3.
    version 1.2 uploaded to nuget.org.

--- Release-Builds/prepare-files-for-release.sh
+++ Release-Builds/prepare-files-for-release.sh
@@ -4,16 +4,39 @@ set -u # exit on using unset variable
 
 CURRENTPATH=`pwd`
 
+
+getos()
+{
+	if [ "$(expr $(uname -s))" = "windows32" ]; then
+		echo "windows"
+	elif [ "$(expr substr $(uname -s) 1 5)" = "MINGW" ]; then
+		echo "windows"
+	else
+	    echo "linux"
+	fi
+}
+
+read osversion junk <<< $(getos; echo $?)
+if [ "$osversion" = 'windows' ];
+then
 # Increment minor number by 1
 # See http://www.codeproject.com/Articles/31236/How-To-Update-Assembly-Version-Number-Automaticall
-mono ./tools/AssemblyInfoUtil.exe -inc:2 "$CURRENTPATH/../LibImages/Properties/AssemblyInfo.cs"
-mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../LibMPlayerCommon/Properties/AssemblyInfo.cs"
-mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../MediaPlayer/Properties/AssemblyInfo.cs"
-mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../LibMPlayerWinform/Properties/AssemblyInfo.cs"
-mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../MPlayerGtkWidget/AssemblyInfo.cs"
-mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../SlideShow/Properties/AssemblyInfo.cs"
-mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../WpfMPlayer/Properties/AssemblyInfo.cs"
-
+    ./tools/AssemblyInfoUtil.exe -inc:2 "$CURRENTPATH/../LibImages/Properties/AssemblyInfo.cs"
+    ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../LibMPlayerCommon/Properties/AssemblyInfo.cs"
+    ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../MediaPlayer/Properties/AssemblyInfo.cs"
+    ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../LibMPlayerWinform/Properties/AssemblyInfo.cs"
+    ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../MPlayerGtkWidget/AssemblyInfo.cs"
+    ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../SlideShow/Properties/AssemblyInfo.cs"
+    ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../WpfMPlayer/Properties/AssemblyInfo.cs"
+else
+    mono ./tools/AssemblyInfoUtil.exe -inc:2 "$CURRENTPATH/../LibImages/Properties/AssemblyInfo.cs"
+    mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../LibMPlayerCommon/Properties/AssemblyInfo.cs"
+    mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../MediaPlayer/Properties/AssemblyInfo.cs"
+    mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../LibMPlayerWinform/Properties/AssemblyInfo.cs"
+    mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../MPlayerGtkWidget/AssemblyInfo.cs"
+    mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../SlideShow/Properties/AssemblyInfo.cs"
+    mono ./tools/AssemblyInfoUtil.exe  -inc:2 "$CURRENTPATH/../WpfMPlayer/Properties/AssemblyInfo.cs"
+fi
 
 VERSION_FULL_LINE=$(grep -F "AssemblyVersion" "$CURRENTPATH/../LibMPlayerCommon/Properties/AssemblyInfo.cs")
 VERSION_WITH_ASTERIK=$(echo $VERSION_FULL_LINE | awk -F\" '{print $(NF-1)}')
