c050a88320ef962fb69ee1eb35491db184304703
Peter Gill <peter@majorsilence.com>
2018-01-24 20:03:54 -0330

    mencoder now must be disposed

--- MplayerUnitTests/Mencoder_Test.cs
+++ MplayerUnitTests/Mencoder_Test.cs
@@ -16,8 +16,10 @@ namespace MplayerUnitTests
         [Test()]
         public void Convert2WebMTest()
         {
-            var a = new LibMPlayerCommon.Mencoder();
-            a.Convert2WebM(GlobalVariables.Video8Path, GlobalVariables.OutputVideoWebM);
+            using (var a = new LibMPlayerCommon.Mencoder())
+            {
+                a.Convert2WebM(GlobalVariables.Video8Path, GlobalVariables.OutputVideoWebM);
+            }
         }
 
         [Test()]
@@ -31,55 +33,68 @@ namespace MplayerUnitTests
         [Test()]
         public void Convert2X264Test()
         {
-            var a = new LibMPlayerCommon.Mencoder();
-            a.Convert2X264(GlobalVariables.Video8Path, GlobalVariables.OutputVideoX264);
+            using (var a = new LibMPlayerCommon.Mencoder())
+            {
+                a.Convert2X264(GlobalVariables.Video8Path, GlobalVariables.OutputVideoX264);
+            }
         }
 
         [Test()]
         public async void Convert2X264AsyncTest()
         {
-            var a = new LibMPlayerCommon.Mencoder();
-            await a.Convert2X264Async(GlobalVariables.Video8Path, GlobalVariables.OutputVideoX264);
+            using (var a = new LibMPlayerCommon.Mencoder())
+            {
+                await a.Convert2X264Async(GlobalVariables.Video8Path, GlobalVariables.OutputVideoX264);
+            }
         }
 
         [Test()]
         public void Convert2DvdMpegPalTest()
         {
-            var a = new LibMPlayerCommon.Mencoder();
-            a.Convert2DvdMpeg(LibMPlayerCommon.Mencoder.RegionType.PAL, GlobalVariables.Video8Path, GlobalVariables.OutputVideoDvdMpegPal);
+            using (var a = new LibMPlayerCommon.Mencoder())
+            {
+                a.Convert2DvdMpeg(LibMPlayerCommon.Mencoder.RegionType.PAL, GlobalVariables.Video8Path, GlobalVariables.OutputVideoDvdMpegPal);
+            }
         }
 
         [Test()]
         public async void Convert2DvdMpegPalAsyncTest()
         {
-            var a = new LibMPlayerCommon.Mencoder();
-            await a.Convert2DvdMpegAsync(LibMPlayerCommon.Mencoder.RegionType.PAL, GlobalVariables.Video8Path, GlobalVariables.OutputVideoDvdMpegPal);
+            using (var a = new LibMPlayerCommon.Mencoder())
+            {
+                await a.Convert2DvdMpegAsync(LibMPlayerCommon.Mencoder.RegionType.PAL, GlobalVariables.Video8Path, GlobalVariables.OutputVideoDvdMpegPal);
+            }
         }
 
         [Test()]
         public void Convert2DvdMpegNtscTest()
         {
-            var a = new LibMPlayerCommon.Mencoder();
-            a.Convert2DvdMpeg(LibMPlayerCommon.Mencoder.RegionType.NTSC, GlobalVariables.Video8Path, GlobalVariables.OutputVideoDvdMpegNtsc);
+            using (var a = new LibMPlayerCommon.Mencoder())
+            {
+                a.Convert2DvdMpeg(LibMPlayerCommon.Mencoder.RegionType.NTSC, GlobalVariables.Video8Path, GlobalVariables.OutputVideoDvdMpegNtsc);
 
-            a.ConversionComplete += a_ConversionComplete;
+                a.ConversionComplete += a_ConversionComplete;
+            }
         }
 
         [Test()]
         public async void Convert2DvdMpegNtscAsyncTest()
         {
-            var a = new LibMPlayerCommon.Mencoder();
-            await a.Convert2DvdMpegAsync(LibMPlayerCommon.Mencoder.RegionType.NTSC, GlobalVariables.Video8Path, GlobalVariables.OutputVideoDvdMpegNtsc);
-
+            using (var a = new LibMPlayerCommon.Mencoder())
+            {
+                await a.Convert2DvdMpegAsync(LibMPlayerCommon.Mencoder.RegionType.NTSC, GlobalVariables.Video8Path, GlobalVariables.OutputVideoDvdMpegNtsc);
+            }
         }
 
         private void a_ConversionComplete(object sender, LibMPlayerCommon.MplayerEvent e)
         {
-            var a = new LibMPlayerCommon.MPlayerDiscover(GlobalVariables.OutputVideoDvdMpegNtsc, GlobalVariables.MplayerPath);
-            Assert.AreEqual(192, a.AudioBitrate);
-            Assert.AreEqual(720, a.Width);
-            Assert.AreEqual(480, a.Height);
-            Assert.AreEqual(48000, a.AudioSampleRate);
+            using (var a = new LibMPlayerCommon.MPlayerDiscover(GlobalVariables.OutputVideoDvdMpegNtsc, GlobalVariables.MplayerPath))
+            {
+                Assert.AreEqual(192, a.AudioBitrate);
+                Assert.AreEqual(720, a.Width);
+                Assert.AreEqual(480, a.Height);
+                Assert.AreEqual(48000, a.AudioSampleRate);
+            }
 
         }
     }
