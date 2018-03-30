98c29adf6c293d0e3cc4ddbd77c88948c681add4
Peter Gill <peter@majorsilence.com>
2018-03-30 11:29:22 -0230

    Test to detect memory leak #15

---
+++ MplayerUnitTests/MPlayer_Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace MplayerUnitTests
+{
+
+    [TestFixture()]
+    public class MPlayer_Test
+    {
+
+        #region Test Video1
+
+        [Test()]
+        public void MemoryLeak_Test()
+        {
+            for (int i = 0; i < 500; i ++) {
+                using (var a = new LibMPlayerCommon.MPlayer (-1, LibMPlayerCommon.MplayerBackends.ASCII, GlobalVariables.MplayerPath)) {
+                    a.Play (GlobalVariables.Video1Path);
+                }
+
+            }
+
+            var processes = System.Diagnostics.Process.GetProcessesByName ("mplayer");
+            Assert.That (processes.Any (), Is.EqualTo (false), $"mplayer process count is {processes.Count()}");
+
+        }
+
+       
+        #endregion
+    }
+}
