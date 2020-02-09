35ab2cff4a3aa2d47f340a959877d628bdbe4b0e
Peter Gill <peter@majorsilence.com>
2020-02-09 17:08:13 -0330

    Set form title to slide show.

--- SlideShow/MainForm.Designer.cs
+++ SlideShow/MainForm.Designer.cs
@@ -57,9 +57,11 @@
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnFileName,
             this.columnFilePath});
-            this.listView1.Location = new System.Drawing.Point(9, 108);
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(14, 166);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(539, 299);
+            this.listView1.Size = new System.Drawing.Size(806, 458);
             this.listView1.TabIndex = 4;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -78,9 +80,10 @@
             // 
             // CreateVideo
             // 
-            this.CreateVideo.Location = new System.Drawing.Point(399, 79);
+            this.CreateVideo.Location = new System.Drawing.Point(598, 122);
+            this.CreateVideo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.CreateVideo.Name = "CreateVideo";
-            this.CreateVideo.Size = new System.Drawing.Size(134, 23);
+            this.CreateVideo.Size = new System.Drawing.Size(201, 35);
             this.CreateVideo.TabIndex = 6;
             this.CreateVideo.Text = "Create Slideshow";
             this.CreateVideo.UseVisualStyleBackColor = true;
@@ -89,18 +92,21 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.AudioFile);
-            this.groupBox2.Location = new System.Drawing.Point(3, 11);
+            this.groupBox2.Location = new System.Drawing.Point(4, 17);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(535, 42);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(802, 65);
             this.groupBox2.TabIndex = 7;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Audio";
             // 
             // AudioFile
             // 
-            this.AudioFile.Location = new System.Drawing.Point(6, 16);
+            this.AudioFile.Location = new System.Drawing.Point(9, 25);
+            this.AudioFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.AudioFile.Name = "AudioFile";
-            this.AudioFile.Size = new System.Drawing.Size(523, 23);
+            this.AudioFile.Size = new System.Drawing.Size(784, 35);
             this.AudioFile.TabIndex = 1;
             this.AudioFile.Text = "Select Audio File";
             this.AudioFile.UseVisualStyleBackColor = true;
@@ -113,17 +119,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 79);
+            this.label1.Location = new System.Drawing.Point(18, 122);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(205, 13);
+            this.label1.Size = new System.Drawing.Size(301, 20);
             this.label1.TabIndex = 8;
             this.label1.Text = "Time in seconds between picture change:";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(223, 77);
+            this.numericUpDown1.Location = new System.Drawing.Point(334, 118);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(180, 26);
             this.numericUpDown1.TabIndex = 9;
             this.numericUpDown1.Value = new decimal(new int[] {
             5,
@@ -141,40 +149,44 @@
             this.panel1.Controls.Add(this.groupBox2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(554, 432);
+            this.panel1.Size = new System.Drawing.Size(831, 665);
             this.panel1.TabIndex = 10;
             // 
             // statusStrip1
             // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripProgressBar1,
             this.toolStripStatusLabel2});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 410);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 620);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(554, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(831, 45);
             this.statusStrip1.TabIndex = 10;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripProgressBar1
             // 
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(150, 37);
             // 
             // toolStripStatusLabel2
             // 
             this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            this.toolStripStatusLabel2.Size = new System.Drawing.Size(256, 17);
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(383, 38);
             this.toolStripStatusLabel2.Text = "This may take several minutes or much longer. ";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(554, 432);
+            this.ClientSize = new System.Drawing.Size(831, 665);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Slide show";
             this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.panel1.ResumeLayout(false);
