590a6ffc5f58223daa53dd1065636b4f3d61b20f
Peter Gill <peter@majorsilence.com>
2020-07-18 11:47:06 -0230

    Rider auto format code.

--- MPlayerGtkWidget/GtkMPlayerWidget.cs
+++ MPlayerGtkWidget/GtkMPlayerWidget.cs
@@ -4,44 +4,43 @@ using LibMPlayerCommon;
 
 namespace MPlayerGtkWidget
 {
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class GtkMPlayerWidget : Gtk.Bin
-	{
-		MPlayer _play;
-		
-		public GtkMPlayerWidget ()
-		{
-			this.Build ();
-		}
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class GtkMPlayerWidget : Gtk.Bin
+    {
+        MPlayer _play;
 
-		protected void OnButtonPlayClicked (object sender, System.EventArgs e)
-		{
-			if (_play != null)
+        public GtkMPlayerWidget()
+        {
+            this.Build();
+        }
+
+        protected void OnButtonPlayCliScked(object sender, System.EventArgs e)
+        {
+            if (_play != null)
             {
                 _play.Stop();
             }
 
-            int handle = (int)this.drawingareaVideo.Handle;
+            int handle = (int) this.drawingareaVideo.Handle;
 
-			if (System.IO.File.Exists(VideoPath) == false  && VideoPath.StartsWith("http") == false)
+            if (System.IO.File.Exists(VideoPath) == false && VideoPath.StartsWith("http") == false)
             {
                 throw new System.IO.FileNotFoundException("File not found", VideoPath);
             }
 
             _play = new MPlayer(handle, MplayerBackends.GL, MPlayerPath);
             _play.Play(VideoPath);
-		}
+        }
 
-		protected void OnButtonStopClicked (object sender, System.EventArgs e)
-		{
-			if (_play != null)
+        protected void OnButtonStopClicked(object sender, System.EventArgs e)
+        {
+            if (_play != null)
             {
                 _play.Stop();
             }
-		}
-		
-		public string MPlayerPath { get; set; }
-        public string VideoPath { get; set; }
-	}
-}
+        }
 
+        public string MPlayerPath { get; set; }
+        public string VideoPath { get; set; }
+    }
+}
