1c44c14f044ecf2386ee197794dd6f56e7388a3f
sonicmouse <sonicmouse@gmail.com>
2020-07-25 15:10:33 -0230

    Gtk Sharp control doesn't work -- here is a fix #20

--- MPlayerGtkWidget/GtkMPlayerWidget.cs
+++ MPlayerGtkWidget/GtkMPlayerWidget.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Runtime.InteropServices;
 using LibMPlayerCommon;
 
 
 namespace MPlayerGtkWidget
 {
+    static class LinuxNative
+    {
+        [DllImport("libgdk-x11-2.0",
+            EntryPoint = "gdk_x11_drawable_get_xid")]
+        [return: MarshalAs(UnmanagedType.I8)]
+        public static extern long GdkX11DrawableGetXID(IntPtr drawable);
+    }
+
+    static class WindowsNative
+    {
+        [DllImport("libgdk-win32-2.0-0.dll",
+            EntryPoint = "gdk_win32_drawable_get_handle",
+            CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr GdkWindowToWin32Window(IntPtr raw);
+    }
+
     [System.ComponentModel.ToolboxItem(true)]
     public partial class GtkMPlayerWidget : Gtk.Bin
     {
@@ -14,21 +31,41 @@ namespace MPlayerGtkWidget
             this.Build();
         }
 
-        protected void OnButtonPlayCliScked(object sender, System.EventArgs e)
+        private static long GetNativeWidgetHandle(Gtk.Widget widget)
+        {
+            var platform = Environment.OSVersion.Platform;
+            if (PlatformID.Unix == platform)
+            {
+                widget.GdkWindow.GetInternalPaintInfo(
+                    out var realDrawable, out var x, out var y);
+                return LinuxNative.GdkX11DrawableGetXID(realDrawable.Handle);
+            }
+
+            if (PlatformID.Win32NT == platform)
+            {
+                var hWnd = WindowsNative.GdkWindowToWin32Window(widget.Handle);
+                return hWnd.ToInt64();
+            }
+
+            throw new PlatformNotSupportedException(
+                "Only tested on Windows and Linux");
+        }
+
+        protected void OnButtonPlayClicked(object sender, System.EventArgs e)
         {
             if (_play != null)
             {
                 _play.Stop();
             }
 
-            int handle = (int) this.drawingareaVideo.Handle;
-
             if (System.IO.File.Exists(VideoPath) == false && VideoPath.StartsWith("http") == false)
             {
                 throw new System.IO.FileNotFoundException("File not found", VideoPath);
             }
 
-            _play = new MPlayer(handle, MplayerBackends.GL, MPlayerPath);
+            var handle = GetNativeWidgetHandle(drawingareaVideo);
+
+            _play = new MPlayer(handle, MplayerBackends.X11, MPlayerPath);
             _play.Play(VideoPath);
         }
 
